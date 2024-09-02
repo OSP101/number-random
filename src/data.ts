a272b6220e7589adf0e98f7499eccfc50850e0c8
OSP101 <supphitan.pk@gmail.com>
2024-09-03 00:09:27 +0700

    update data SA-SEC01 ASS03

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,4 @@
 import { getRandomColor } from './color'
-// import { faker } from '@faker-js/faker'
 
 export interface RandomName {
   name: string
@@ -7,16 +6,10 @@ export interface RandomName {
   color: string
 }
 
-const selectedName: string[] = ['G01','G07','G04','G05','G06','G08','G02','G03','G10']
+const selectedName: string[] = ['G01','G07','G04','G05','G06','G08','G02','G03']
 const selectedOther: string[] = ['เพิ่นบ่แม่นผู้สาวเฮา','อ้ายบ่แมนเขา','ยังฮักไผอีกได้บ่','อ้ายมาส่งทาง','ริมฝั่งหนองหาน','อ้ายฮักเจ้าเด้อ','เป็นเกียรติหลาย','รำคาญกะบอกกันเด้อ','หลมปากเว้า','สิกอดแบบบ่ถาม','สิมาฮักหยังตอนนี้','กอดคนนอกใจ','ทนบ่ไหวทางใครทางมัน','นางฟ้าหรือยาพิษ','ใจบ่มักดี','ดาวมีไว้เบิ่ง','ชิมิ','โทรหาครั้งสุดท้าย','สะดวกคุยหรือเปล่า','คำว่าจบพูดเบาๆ ก็เจ็บ','เชื่อใจเธอแต่ไม่ไว้ใจเขา','ไม่ใช่แฟนทำแทนไม่ได้','แฟนเก็บ','คำพิพากษา','ใจอ้ายเจ็บ','สมพื้น','เรื่องธรรมดา','ไม่ให้ไป','ไปเถอะเธอ','ถ้าเธอรักฉันจริง','เปิดตัวเขา']
 
-// export function createRandomName(): RandomName {
-//   return {
-//     name: faker.person.firstName(),
-//     selected: false,
-//     color: getRandomColor(),
-//   }
-// }
+const randomizedSelectedName = selectedName.sort(() => Math.random() - 0.5)
 
 export const nametest: RandomName[] = selectedOther.map(name => ({
   name,
@@ -24,7 +17,7 @@ export const nametest: RandomName[] = selectedOther.map(name => ({
   color: getRandomColor(),
 }))
 
-export const names: RandomName[] = selectedName.map(name => ({
+export const names: RandomName[] = randomizedSelectedName.map(name => ({
   name,
   selected: true,
   color: getRandomColor(),
