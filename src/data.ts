7c4497f2610becb54acb0cf639ff74f89fc12154
OSP101 <supphitan.pk@gmail.com>
2024-09-04 12:39:32 +0700

    update SA-SEC2 ASS03 IT

--- src/data.ts
+++ src/data.ts
@@ -7,7 +7,7 @@ export interface RandomName {
 }
 
 const selectedName: string[] = ['G09','G10','G11','G12','G13']
-const selectedOther: string[] = ['Docker', 'Kubernetes', 'Jenkins', 'CircleCI', 'GitLab CI', 'Terraform', 'Ansible','NGINX', 'Apache','JavaScript','React.js', 'Vue.js', 'Angular', 'Svelte', 'Next.js', 'Nuxt.js','Python','PHP' ,'Laravel','Java','Spring Boot']
+const selectedOther: string[] = ['Docker', 'Kubernetes', 'Jenkins', 'CircleCI', 'GitLab CI', 'Node.js','Terraform', 'Ansible','NGINX', 'Apache','JavaScript','React.js', 'Vue.js', 'Angular', 'Svelte', 'Next.js', 'Nuxt.js','Python','PHP' ,'Laravel','Java','Spring Boot']
 
 const randomizedSelectedName = selectedName.sort(() => Math.random() - 0.5)
 
